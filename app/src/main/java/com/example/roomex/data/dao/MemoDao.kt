36c6c88317d5215141804f3ab304d1e224d95cce
SeunghHun <starzary@naver.com>
2022-12-01 15:55:57 +0900

    Edit MemoRepository.kt, MemoDao.kt

--- app/src/main/java/com/example/roomex/data/dao/MemoDao.kt
+++ app/src/main/java/com/example/roomex/data/dao/MemoDao.kt
@@ -11,12 +11,15 @@ interface MemoDao {
     @Delete
     suspend fun deleteMemo(memo: Memo)
 
+    // Memo 테이블의 id와 일치하는 id를 가진 데이터를 삭제한다.
     @Query("DELETE FROM Memo Where id = :id")
     suspend fun deleteMemoByID(id: Long)
 
+    // Memo 테이블의 모든 데이터를 반환한다.
     @Query("SELECT * FROM Memo")
     suspend fun getAllMemo(): List<Memo>
 
+    // Memo 테이블에서 id 필드의 값이 'id'인 모든 레코드의 memo 값을 memo 변경한다.
     @Query("UPDATE Memo SET memo = :memo WHERE id = :id")
     suspend fun modifyMemo(id: Long, memo: String)
 }
--- app/src/main/java/com/example/roomex/data/respository/MemoRepository.kt
+++ app/src/main/java/com/example/roomex/data/respository/MemoRepository.kt
@@ -1,4 +1,14 @@
 package com.example.roomex.data.respository
 
+import com.example.roomex.common.GlobalApplication
+import com.example.roomex.data.model.Memo
+
 class MemoRepository {
+    private val appDBInstance = GlobalApplication.appDataBaseInstance.memoDao()
+
+    suspend fun insertMemo(memo: Memo) = appDBInstance.insertMemo(memo)
+    suspend fun deleteMemo(memo: Memo) = appDBInstance.deleteMemo(memo)
+    suspend fun deleteMemoByID(id: Long) = appDBInstance.deleteMemoByID(id)
+    suspend fun getAllMemo() = appDBInstance.getAllMemo()
+    suspend fun modifyMemo(id: Long, memo: String) = appDBInstance.modifyMemo(id, memo)
 }
