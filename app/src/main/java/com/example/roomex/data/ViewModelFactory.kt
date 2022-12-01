d3ea88cb2e7dd267d1660e6a3ba70d5243ca97c2
SeunghHun <starzary@naver.com>
2022-12-01 21:09:00 +0900

    Edit Memo.kt, ViewModelFactory.kt

--- app/src/main/java/com/example/roomex/data/ViewModelFactory.kt
+++ app/src/main/java/com/example/roomex/data/ViewModelFactory.kt
@@ -1,4 +1,11 @@
 package com.example.roomex.data
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.roomex.data.respository.MemoRepository
+
+class ViewModelFactory(private val memoRepository: MemoRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MemoRepository::class.java).newInstance(memoRepository)
+    }
 }
--- app/src/main/java/com/example/roomex/data/model/Memo.kt
+++ app/src/main/java/com/example/roomex/data/model/Memo.kt
@@ -3,8 +3,12 @@ package com.example.roomex.data.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+// 메모장 정보를 표현하는 Entity를 정의한다.
+// Entity 이름과 동일한 Memo 테이블이 생성된다.
 @Entity
 class Memo (
+    // id를 주요 키로 사용한다.
+    // id를 자동으로 지정
     @PrimaryKey(autoGenerate = true) var id: Long,
     var memo: String,
     var editMode: Boolean
