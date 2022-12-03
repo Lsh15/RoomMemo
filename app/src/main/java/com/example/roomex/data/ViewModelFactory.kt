bcf4aad265bc2be87f8431c782d5a0e45b029009
SeunghHun <starzary@naver.com>
2022-12-03 10:40:37 +0900

    Edit MemoDiffUtil.kt, ViewModelFactory.kt

--- app/src/main/java/com/example/roomex/data/ViewModelFactory.kt
+++ app/src/main/java/com/example/roomex/data/ViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.roomex.data.respository.MemoRepository
 
 class ViewModelFactory(private val memoRepository: MemoRepository): ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(MemoRepository::class.java).newInstance(memoRepository)
     }
 }
--- app/src/main/java/com/example/roomex/util/MemoDiffUtil.kt
+++ app/src/main/java/com/example/roomex/util/MemoDiffUtil.kt
@@ -1,4 +1,22 @@
 package com.example.roomex.util
 
-class MemoDiffUtil {
+import androidx.recyclerview.widget.DiffUtil
+import com.example.roomex.data.model.Memo
+
+class MemoDiffUtil(val oldList: MutableList<Memo>, val newList: MutableList<Memo>): DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].id == newList[newItemPosition].id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
 }
